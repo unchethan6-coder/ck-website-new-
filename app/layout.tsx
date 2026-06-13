f1bd1551d91eebdf5537258ea470b1780f78e3c3
v0agent <it+v0agent@vercel.com>
2026-06-13 18:45:01 +0000

    Add Meta Pixel tracking code 1102958381163703
    - Added Meta Pixel (Facebook Pixel) conversion tracking script to layout.tsx
    - Configured with pixel ID 1102958381163703 for tracking page views and conversions
    - Includes noscript fallback for browsers with JavaScript disabled
    - Enables Facebook audience tracking and remarketing capabilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -190,25 +190,34 @@ export default function RootLayout({
           }}
         />
 
-        {/* Meta Pixel */}
-        {process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID ? (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
-                fbq('track', 'PageView');
-              `,
-            }}
+        {/* Meta Pixel - 1102958381163703 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1102958381163703');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1102958381163703&ev=PageView&noscript=1"
+            alt=""
           />
-        ) : null}
+        </noscript>
+
+        {/* Meta Pixel - Conditional */}
 
         {/* Structured Data - Organization Schema */}
         <script
